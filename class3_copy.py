c6e7ce4e62b7defef047686c2151516c24528fdc
komal-vk <48751652+komal-vk@users.noreply.github.com>
2019-09-25 08:48:23 +0530

    moved files

--- class3_copy.py
+++
@@ -1,9 +0,0 @@
-
-class copy (TITLE_OBJECT):
-    BARCODE = None
-    BORROWER_ID = None
-    ISSUE_DATE = None
-    RETURN_DATE = None
-    ACTUAL_RETURN_DATE = None
-    ROW_NO = None
-    COLUMN_NO = None
